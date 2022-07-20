09a0ebba21c560d22fb62ff9f6c6fc23a25ef8a4
Donalson <donalson1517@gmail.com>
2022-07-20 17:34:53 -0600

    Se reforzo la utilizacion de las rutas para empezar a darle forma al login

--- src/app.py
+++ src/app.py
@@ -1,14 +1,23 @@
 #Importaciones de paquetes/librerias y de otros archivos
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from config import config
 
 #Se instancia la aplicacion de flask
 app = Flask(__name__)
 
 #Rutas
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('auth/login.html')
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['usuario'])
+        print(request.form['contraseña'])
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
 
 #Se inicializa la aplicacion de flask y se le pasan algunos parametros
 if __name__ == '__main__':
