e40ed7596216ac387367bcab05385009cb174e47
Donalson <donalson1517@gmail.com>
2022-07-20 17:06:16 -0600

    Se agrego los documentos hmtl base para empezar a trabajar y la primer ruta de prueba

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 #Importaciones de paquetes/librerias y de otros archivos
-from flask import Flask
+from flask import Flask, render_template
 from config import config
 
 #Se instancia la aplicacion de flask
 app = Flask(__name__)
+
+#Rutas
+@app.route('/')
+def index():
+    return render_template('auth/login.html')
 
 #Se inicializa la aplicacion de flask y se le pasan algunos parametros
 if __name__ == '__main__':
